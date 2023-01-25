48b886e7816a77c0cf76377a391b88526c6a1c5b
Emad Jacob Maroun <emoun.open@gmail.com>
2023-01-25 18:05:55 +0100

    Updated checking and evaluating

--- bench/checkBenchmark.sh
+++ bench/checkBenchmark.sh
@@ -10,32 +10,26 @@ fi
 if [ $CHOICE -eq 0 ]; then
 	echo "Checking Traditional"
 	POSTFIX=""
-	OPTIONS="-O2 -mllvm --mpatmos-serialize=a.pml"
 fi
 if [ $CHOICE -eq 1 ]; then
-	echo "Checking Single-path"
-	POSTFIX="-sp"
-	OPTIONS="-O2"
+	echo "Checking Traditional Dual-Issue"
+	POSTFIX="-di"
 fi
 if [ $CHOICE -eq 2 ]; then
-	echo "Checking CET Opposite"
-	POSTFIX="-cet-opposite"
-	OPTIONS="-O2 -mllvm --mpatmos-enable-cet=opposite"
+	echo "Checking Single-Path"
+	POSTFIX="-sp"
 fi
 if [ $CHOICE -eq 3 ]; then
-	echo "Checking CET Counter"
-	POSTFIX="-cet-counter"
-	OPTIONS="-O2 -mllvm --mpatmos-enable-cet=counter"
+	echo "Checking Single-Path Dual-Issue"
+	POSTFIX="-sp-di"
 fi
 if [ $CHOICE -eq 4 ]; then
-	echo "Checking CET hybrid"
-	POSTFIX="-cet-hybrid"
-	OPTIONS="-O2 -mllvm --mpatmos-enable-cet"
+	echo "Checking CET"
+	POSTFIX="-cet"
 fi
 if [ $CHOICE -eq 5 ]; then
-	echo "Checking CET hybrid No-Pseudo"
-	POSTFIX="-cet-pseudo"
-	OPTIONS="-O2 -mllvm --mpatmos-enable-cet -mllvm --mpatmos-disable-pseudo-roots"
+	echo "Checking CET Dual-Issue"
+	POSTFIX="-cet-di"
 fi
 
 COMPILER=patmos-clang
@@ -72,10 +66,6 @@ for dir in */; do
 				for I in {1..1} ; do
 					if [ -f "a$POSTFIX.out" ]; then
 						rm "a$POSTFIX.out"
-						rm -f "a$POSTFIX-1.out"
-						rm -f "a$POSTFIX-2.out"
-						rm -f "a$POSTFIX-4.out"
-						rm -f "a$POSTFIX-8.out"
 						rm -f "a$POSTFIX.stats"
 						rm -f "a$POSTFIX.pml"
 						rm -f "a$POSTFIX.wcet"
@@ -88,35 +78,32 @@ for dir in */; do
 					ENTRYFN=$(grep -E "_Pragma[[:space:]]*\([[:space:]]*\"entrypoint\"" -r * | cut -d ")" -f2 | cut -d "(" -f1)
 					ENTRYFN=${ENTRYFN//[[:blank:]]/}
 					
-					if [ -z "$POSTFIX" ]; then
-						SP_FUN_OPT=""
-						SP_ADD_OPTIONS=""
-					else
-						SP_FUN_OPT="-mllvm --mpatmos-singlepath=$ENTRYFN"
-						SP_ADD_OPTIONS="-mllvm --stats -mllvm --info-output-file=a$POSTFIX.stats"
+					FULL_OPTIONS=""
+					if [[ "$POSTFIX" == *"-di"* ]]; then
+						FULL_OPTIONS="-mllvm --mpatmos-disable-vliw=false"
 					fi
-					
+					if [[ "$POSTFIX" == *"-sp"* || "$POSTFIX" == *"-cet"* ]]; then
+						FULL_OPTIONS="$FULL_OPTIONS -mllvm --mpatmos-singlepath=$ENTRYFN"
+					fi
+					if [[ "$POSTFIX" == *"-cet"* ]]; then
+						FULL_OPTIONS="$FULL_OPTIONS -mllvm --mpatmos-enable-cet"
+					else
+						FULL_OPTIONS="$FULL_OPTIONS -mllvm --mpatmos-serialize=a$POSTFIX.pml"
+					fi
+										
 					#set -x
 					# Please remove '&>/dev/null' to identify the warnings (if any)
-					timeout 300 $COMPILER $OPTIONS $SP_FUN_OPT *.c -o "a$POSTFIX.out" $SP_ADD_OPTIONS #&>/dev/null
+					timeout 300 $COMPILER -O2 $FULL_OPTIONS *.c -o "a$POSTFIX.out" -mllvm --stats -mllvm --info-output-file="a$POSTFIX.stats" #&>/dev/null
 										
 					if [ -f "a$POSTFIX.out" ]; then
 						timeout 1800 $EXEC "./a$POSTFIX.out" -V 2> "./a$POSTFIX.pasim" #&>/dev/null
 						RETURNVALUE=$(echo $?)
 						if [ $RETURNVALUE -eq 0 ]; then 
-							if [ -z "$POSTFIX" ]; then
-								platin wcet -i a.pml -b a.out -e $ENTRYFN --report > a.wcet 2>&1
-							fi
-							if [ "$POSTFIX" == "-cet-hybrid" ]; then
-								timeout 300 $COMPILER $OPTIONS $SP_FUN_OPT *.c -o "a$POSTFIX-1.out" -mllvm --mpatmos-cet-compensation-function=__patmos_main_mem_access_compensation1
-								timeout 300 $COMPILER $OPTIONS $SP_FUN_OPT *.c -o "a$POSTFIX-2.out" -mllvm --mpatmos-cet-compensation-function=__patmos_main_mem_access_compensation2
-								timeout 300 $COMPILER $OPTIONS $SP_FUN_OPT *.c -o "a$POSTFIX-4.out" -mllvm --mpatmos-cet-compensation-function=__patmos_main_mem_access_compensation4
-								timeout 300 $COMPILER $OPTIONS $SP_FUN_OPT *.c -o "a$POSTFIX-8.out" -mllvm --mpatmos-cet-compensation-function=__patmos_main_mem_access_compensation8
-								
-								timeout 1800 $EXEC "./a$POSTFIX-1.out" -V 2> "a$POSTFIX-1.pasim"
-								timeout 1800 $EXEC "./a$POSTFIX-2.out" -V 2> "a$POSTFIX-2.pasim"
-								timeout 1800 $EXEC "./a$POSTFIX-4.out" -V 2> "a$POSTFIX-4.pasim"
-								timeout 1800 $EXEC "./a$POSTFIX-8.out" -V 2> "a$POSTFIX-8.pasim"
+							if [[ "$POSTFIX" == *"-cet"* ]]; then
+								BOUND=$(python3 ../../find_wcet.py "./a$POSTFIX.pasim" $ENTRYFN)
+								echo "best WCET bound: $BOUND" > "a$POSTFIX.wcet"
+							else
+								platin wcet -i "a$POSTFIX.pml" -b "a$POSTFIX.out" -e $ENTRYFN --report > "a$POSTFIX.wcet" 2>&1
 							fi
 							break
 						fi						
--- bench/evaluate-all.sh
+++ bench/evaluate-all.sh
@@ -10,99 +10,64 @@ for dir in */; do
 
     printf "Entering ${dir} \n"
 	
-	for BENCH in */; do
-		cd "$BENCH"
-				
-		printf "Checking ${BENCH} ..."
-		
-		if [ -f a.out ]; then
-			if [ -f a-sp.out ]; then
-				if [ -f a-cet-opposite.out ]; then
-					if [ -f a-cet-counter.out ]; then
-						if [ -f a-cet-hybrid.out ]; then
-							BENCHNAME=${BENCH::-1} #Remove the last '/'
-							
-							ENTRYFN=$(grep -E "_Pragma[[:space:]]*\([[:space:]]*\"entrypoint\"" -r * | cut -d ")" -f2 | cut -d "(" -f1)
-							ENTRYFN=${ENTRYFN//[[:blank:]]/}
-							
-							STRING=$($EXTRACT_WCET a.pasim $ENTRYFN)
-							echo "${BENCHNAME}Trad: $STRING" >> ../../result.txt 
-
-							cat a.wcet | grep "best WCET bound:" >/dev/null 2>&1
-							RET=$(echo $?)
-							if [ $RET -eq 0 ]; then 
-								STRING=$(cat a.wcet | grep "best WCET bound:")
-								echo "${BENCHNAME}TradWcet: ${STRING//[^0-9]/}" >> ../../result.txt 
-
-								STRING=$(cat a.wcet | grep "cache-unknown-address-data:")
-								echo "${BENCHNAME}TradCacheMiss: ${STRING//[^0-9]/}" >> ../../result.txt 
-							fi
-
-							STRING=$($EXTRACT_WCET a-sp.pasim  $ENTRYFN)
-							echo "${BENCHNAME}Sp: $STRING" >> ../../result.txt  
-
-							pasim a-sp.out -D no -V 2> a-sp-nocache.pasim
-							STRING=$($EXTRACT_WCET a-sp-nocache.pasim $ENTRYFN)
-							echo "${BENCHNAME}SpNoCache: $STRING" >> ../../result.txt  
-
-							STRING=$($EXTRACT_WCET a-cet-opposite.pasim  $ENTRYFN)
-							echo "${BENCHNAME}CetOpposite: $STRING" >> ../../result.txt 
-							STRING=$(cat a-cet-opposite.stats | grep "patmos-const-exec        - Number of non-phi instructions added by the 'opposite' constant execution time compensation")
-							echo "${BENCHNAME}CetOppositeInstrs: ${STRING//[^0-9]/}" >> ../../result.txt 
-
-							STRING=$($EXTRACT_WCET a-cet-counter.pasim  $ENTRYFN)
-							echo "${BENCHNAME}CetCounter: $STRING" >> ../../result.txt  
-							STRING=$(cat a-cet-counter.stats | grep "patmos-const-exec        - Number of non-phi instructions added by the 'counter' compensation algorithm")
-							echo "${BENCHNAME}CetCounterInstrs: ${STRING//[^0-9]/}" >> ../../result.txt 
-
-							STRING=$($EXTRACT_WCET a-cet-hybrid.pasim  $ENTRYFN)
-							echo "${BENCHNAME}CetHybrid: $STRING" >> ../../result.txt  
-							STRING=$(cat a-cet-hybrid.stats | grep "patmos-const-exec        - Number of functions using 'opposite' algorithm for constant execution time")
-							echo "${BENCHNAME}CetHybridOppositeFuns: ${STRING//[^0-9]/}" >> ../../result.txt 
-							STRING=$(cat a-cet-hybrid.stats | grep "patmos-const-exec        - Number of non-phi instructions added by the 'opposite' constant execution time compensation")
-							echo "${BENCHNAME}CetHybridOppositeInstrs: ${STRING//[^0-9]/}" >> ../../result.txt 
-							STRING=$(cat a-cet-hybrid.stats | grep "patmos-const-exec        - Number of functions using 'counter' algorithm for constant execution time")
-							echo "${BENCHNAME}CetHybridCounterFuns: ${STRING//[^0-9]/}" >> ../../result.txt 
-							STRING=$(cat a-cet-hybrid.stats | grep "patmos-const-exec        - Number of non-phi instructions added by the 'counter' compensation algorithm")
-							echo "${BENCHNAME}CetHybridCounterInstrs: ${STRING//[^0-9]/}" >> ../../result.txt 
-							STRING=$(cat a-cet-hybrid.stats | grep "patmos-const-exec        - Number of functions not needing any compensation for constant execution time")
-							echo "${BENCHNAME}CetHybridNoCompFuns: ${STRING//[^0-9]/}" >> ../../result.txt 
-							STRING=$(cat a-cet-hybrid.stats | grep "patmos-singlepath        - Number of functions used in single-path")
-							echo "${BENCHNAME}CetHybridSPFuns: ${STRING//[^0-9]/}" >> ../../result.txt 
-							STRING=$(cat a-cet-hybrid.stats | grep "patmos-singlepath        - Number of functions marked as pseudo-root")
-							echo "${BENCHNAME}CetHybridPseudoFuns: ${STRING//[^0-9]/}" >> ../../result.txt 
-							STRING=$($EXTRACT_WCET a-cet-hybrid-1.pasim  $ENTRYFN)
-							echo "${BENCHNAME}CetHybridOne: $STRING" >> ../../result.txt  
-							STRING=$($EXTRACT_WCET a-cet-hybrid-2.pasim  $ENTRYFN)
-							echo "${BENCHNAME}CetHybridTwo: $STRING" >> ../../result.txt  
-							STRING=$($EXTRACT_WCET a-cet-hybrid-4.pasim  $ENTRYFN)
-							echo "${BENCHNAME}CetHybridFour: $STRING" >> ../../result.txt  
-							STRING=$($EXTRACT_WCET a-cet-hybrid-8.pasim  $ENTRYFN)
-							echo "${BENCHNAME}CetHybridEight: $STRING" >> ../../result.txt  
-																													
-							echo "" >> ../../result.txt 
-						else
-							echo "No CET-Hybrid"
-						fi
-					else
-						echo "No CET-Counter"
-					fi
-				else
-					echo "No CET-Instr"
-				fi
+	if [ -f patmos-ignore.txt ]; then
+		echo "Ignored"
+	else
+		for BENCH in */; do
+			cd "$BENCH"
+					
+			printf "Checking ${BENCH} ..."
+			
+			if [ -f patmos-ignore.txt ]; then
+				printf "Ignored\n"
 			else
-				echo "No single-path"
+				BENCHNAME=${BENCH::-1} #Remove the last '/'
+				
+				if [ -f a.wcet ]; then
+					STRING=$(cat a.wcet | grep "best WCET bound:")
+					echo "${BENCHNAME}Trad: ${STRING//[^0-9]/}" >> ../../result.txt 
+				else
+					echo "Missing a.wcet"
+				fi
+				if [ -f a-di.wcet ]; then
+					STRING=$(cat a-di.wcet | grep "best WCET bound:")
+					echo "${BENCHNAME}TradDI: ${STRING//[^0-9]/}" >> ../../result.txt 
+				else
+					echo "Missing a-di.wcet"
+				fi
+				if [ -f a-sp.wcet ]; then
+					STRING=$(cat a-sp.wcet | grep "best WCET bound:")
+					echo "${BENCHNAME}SP: ${STRING//[^0-9]/}" >> ../../result.txt 
+				else
+					echo "Missing a-sp.wcet"
+				fi
+				if [ -f a-sp-di.wcet ]; then
+					STRING=$(cat a-sp-di.wcet | grep "best WCET bound:")
+					echo "${BENCHNAME}SPDI: ${STRING//[^0-9]/}" >> ../../result.txt 
+				else
+					echo "Missing a-sp-di.wcet"
+				fi
+				if [ -f a-cet.wcet ]; then
+					STRING=$(cat a-cet.wcet | grep "best WCET bound:")
+					echo "${BENCHNAME}CET: ${STRING//[^0-9]/}" >> ../../result.txt 
+				else
+					echo "Missing a-cet.wcet"
+				fi
+				if [ -f a-cet-di.wcet ]; then
+					STRING=$(cat a-cet-di.wcet | grep "best WCET bound:")
+					echo "${BENCHNAME}CETDI: ${STRING//[^0-9]/}" >> ../../result.txt 
+				else
+					echo "Missing a-cet-di.wcet"
+				fi
+				echo "" >> ../../result.txt 
 			fi
-		else
-			echo "No compiles"
-		fi 
-		echo ""
-		cd ..
-	done
-	
-    printf "Leaving ${dir} \n\n"
-    
-    cd ..
-done
+					
+			echo ""
+			cd ..
+		done
+		
+	fi
+	printf "Leaving ${dir} \n\n"
 
-echo "PASS: $PASS, FAIL_COMP: $FAIL_COMP, FAIL_EXEC: $FAIL_EXEC, IGNORED: $IGNORE"
+	cd ..
+done
