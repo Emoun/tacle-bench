e1f5e6a697a7cf882fd0388e0449cf99d92dcf3d
Emad Jacob Maroun <emoun.open@gmail.com>
2023-02-06 18:06:18 +0100

    Evaluation now also gets running time of analyzed configuration to show whether analysis result is definately wrong.

--- bench/evaluate-all.sh
+++ bench/evaluate-all.sh
@@ -22,26 +22,34 @@ for dir in */; do
 				printf "Ignored\n"
 			else
 				BENCHNAME=${BENCH::-1} #Remove the last '/'
-				
+				ENTRYFN=${BENCHNAME}_main
 				if [ -f a.wcet ]; then
+					EXEC_TIME=$(python3 ../../find_wcet.py "./a.pasim" $ENTRYFN)
+					echo "${BENCHNAME}TradExec: ${EXEC_TIME//[^0-9]/}" >> ../../result.txt 
 					STRING=$(cat a.wcet | grep "best WCET bound:")
 					echo "${BENCHNAME}Trad: ${STRING//[^0-9]/}" >> ../../result.txt 
 				else
 					echo "Missing a.wcet"
 				fi
 				if [ -f a-di.wcet ]; then
+					EXEC_TIME=$(python3 ../../find_wcet.py "./a-di.pasim" $ENTRYFN )
+					echo "${BENCHNAME}TradDIExec: ${EXEC_TIME//[^0-9]/}" >> ../../result.txt 
 					STRING=$(cat a-di.wcet | grep "best WCET bound:")
 					echo "${BENCHNAME}TradDI: ${STRING//[^0-9]/}" >> ../../result.txt 
 				else
 					echo "Missing a-di.wcet"
 				fi
 				if [ -f a-sp.wcet ]; then
+					EXEC_TIME=$(python3 ../../find_wcet.py "./a-sp.pasim" $ENTRYFN )
+					echo "${BENCHNAME}SPExec: ${EXEC_TIME//[^0-9]/}" >> ../../result.txt 
 					STRING=$(cat a-sp.wcet | grep "best WCET bound:")
 					echo "${BENCHNAME}SP: ${STRING//[^0-9]/}" >> ../../result.txt 
 				else
 					echo "Missing a-sp.wcet"
 				fi
 				if [ -f a-sp-di.wcet ]; then
+					EXEC_TIME=$(python3 ../../find_wcet.py "./a-sp-di.pasim" $ENTRYFN )
+					echo "${BENCHNAME}SPDIExec: ${EXEC_TIME//[^0-9]/}" >> ../../result.txt 
 					STRING=$(cat a-sp-di.wcet | grep "best WCET bound:")
 					echo "${BENCHNAME}SPDI: ${STRING//[^0-9]/}" >> ../../result.txt 
 				else
